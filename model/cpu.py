ae009886732769ace1dbfc6fdc0f6fecaba0dccd
Pranav Kulkarni <pranavkrishnaji.kulkarni@gmail.com>
2021-08-21 13:46:44 -0600

    migrated existing code to new bit manip API

--- model/cpu.py
+++ model/cpu.py
@@ -1,217 +1,23 @@
 #!/usr/bin/env python3
-from numpy.core.numeric import binary_repr
 from elf_decode.elf_decode import ELF_DECODE
 from typing import List
 from abc import ABC, abstractmethod
-from bit_manipulation import BitManip_32 as bm
+from bit_manipulation import BitManip32 as bm
+from numpy import uint32, uint64
 
-class CPU:
+class CPU():
 	
 	def __init__(self, base_isa: str):
-		if base_isa == "rv32i":
-			isa = RV32I();
-
-class RV32I:
-	""" This class defines the RV32I compatible CPU with built-in memory structure"""
-
-	PC: int = 0
-	REG_FILE: List[int] = [0] * 32
-	xlen: int = 32
-
-	#TODO: Add a description of this class	
-	def __init__(self, mem_size : int):
-		self.mem_size = mem_size
-		self.address_space = Memory_32(self.mem_size)
-
-	def __incr_PC(self, factor=0x4):
-		# Store a temporary PC
-		PC = self.PC
-
-		self.PC+=factor
-		if self.PC >= self.mem_size:
-			self.PC = int(0)
-
-		# Return old PC 
-		return PC
-
-	def st_run(self, assm_code, test_cases):
-		pass
-			
-	def __fetcher(self):
-		return self.address_space[self.__incr_PC()]
-				
-	def __decoder(self, inst_hex):
-		return RV32I_INSTR(inst_hex)
-
-	def __execute(self, inst_32i):
-		instr = inst_32i.bits
-
-		# Conditional Branch
-		if inst_32i.opcode == "1100011":
-			self.__execute_branch(inst_32i)
-		
-		# Jump and link register
-		elif inst_32i.opcode == "1100111":
-			offset = inst_32i.get_int_bits(instr, 31, 20)
-			src = inst_32i.get_int_bits(instr, 19, 15)
-			dst = inst_32i.get_int_bits(instr, 11, 7)
-			self.REG_FILE[dst] = self.PC + 4
-			self.PC = offset + src
-			
-		# Jump and link 
-		elif inst_32i.opcode == "1101111":
-			offset = int(inst_32i.concat_bits(instr, 31, [19, 12], 20, [30, 21]), 2)
-			dst = inst_32i.get_int_bits(instr, 11, 7)
-			self.REG_FILE[dst] = self.PC + 4
-			self.__incr_PC(offset)
-
-		# Load upper immediate
-		elif inst_32i.opcode == "0110111" or inst_32i.opcode == "0010111":
-			u_imm = int((inst_32i.get_bits(instr, 31, 12) + 12*"0"), 2)
-			dst = inst_32i.get_int_bits(instr, 11, 7)
-			if inst_32i.opcode == "0110111":
-				self.REG_FILE[dst] = u_imm
-			else: 
-				self.__incr_PC(u_imm)
-		
-		# Integer register-immediate instructions
-		elif inst_32i.opcode == "0010011":
-			self.__execute_int_imm_reg(inst_32i)
-		
-		# Integer register-register instructions
-		elif inst_32i.opcode == "0110011":
-			self.__execute_int_reg_reg(inst_32i)
-
-		# Load instructions
-		elif inst_32i.opcode == "0000011":
-			self.__execute_load(inst_32i)
-		
-		elif inst_32i.opcode == "0100011":
-			self.__execute_store(inst_32i)
-			
-	def __execute_load(self, inst_32i):
-		instr = inst_32i.bits
-		imm = inst_32i.get_sign_extended_int_bits(instr, 31, 20, len(instr))
-		src1 = inst_32i.get_int_bits(instr, 19, 15)
-		dest = inst_32i.get_int_bits(instr, 11, 7)
-		funct3 = int(inst_32i.funct3, 2)
-		effective_address = self.REG_FILE[src1] + imm
-
-		if funct3 == 0:
-			self.REG_FILE[dest] = self.address_space[effective_address]
-		
-		elif funct3 == 1:
-			#TODO: implement a memory class
-			pass
-
-	def __execute_store(self, inst_32i):
-		#TODO: Finish this routine
-		pass
-		
-	def __execute_int_imm_reg(self, inst_32i):
-		instr = inst_32i.bits
-		funct3 = int(inst_32i.funct3, 2)
-		imm_arith = inst_32i.get_int_bits(instr, 31, 20)
-		imm_shift = inst_32i.get_int_bits(instr, 31, 25)
-		shamt = inst_32i.get_int_bits(instr, 24, 20)
-		src = inst_32i.get_int_bits(instr, 19, 15)
-		dest = inst_32i.get_int_bits(instr, 11, 7)
-
-		if funct3 == 0:
-			self.REG_FILE[dest] = self.REG_FILE[src] + self.REG_FILE[imm]
-			
-		elif funct3 == 1:	
-			self.REG_FILE[dest] = self.REG_FILE[src] << shamt
-		
-		elif funct3 == 2:
-			self.REG_FILE[dest] = self.REG_FILE[src] 
-		#TODO: Finish this routine
-
-
-	def __execute_int_reg_reg(self, inst_32i):
-		instr = inst_32i.bits
-		src2 = inst_32i.get_int_bits(instr, 24, 20)
-		src1 = inst_32i.get_int_bits(instr, 19, 15)
-		dest = inst_32i.get_int_bits(instr, 11, 7)
-		#TODO: Finish this routine
-
-	def __execute_branch(self, inst_32i):
-		instr = inst_32i.bits
-		opcode = inst_32i.opcode
-		funct3 = int(inst_32i.funct3, 2)
-		offset = inst_32i.concat_bits(instr, 31, 7, [30, 25], [11, 8])
-		src1 = int(inst_32i.get_bits(instr, 19, 15), 2)
-		src2 = int(inst_32i.get_bits(instr, 24, 20), 2)
-
-		if self.REG_FILE[src1] == self.REG_FILE[src2] and funct3 == 0:
-			self.__incr_PC(self.offset)
-
-		elif self.REG_FILE[src1] != self.REG_FILE[src2] and funct3 == 1:
-			self.__incr_PC(self.offset)
-
-		elif funct3 == 4 or funct3 == 6:
-			if self.REG_FILE[src1] < self.REG_FILE[src2]:
-				self.incr_PC(self.offset)
-		
-		elif funct3 == 5 or funct3 == 7:
-			if self.REG_FILE[src1] > self.REG_FILE[src2]:
-				self.incr_PC[self.offset]
-
-
-			
-	def __data_memory_access(self, address, data):
-		pass
-	
-	def __write_back(self, address, data):
 		pass
 
 
-class RV32I_INSTR:
-	#TODO: Add a description of this class	
-	def __init__(self, hex_str):
-		self.bm = BitManip()
-		self.bits = self.hex2bin(hex_str)
-		self.opcodes = self.get_bits(self.bits, 6, 0) 
-		self.funct3 = self.get_bits(self.bits, 14, 12)
-		self.variant = self.concat_bits(self.bits, [14, 12], [6, 0])
+class InstrExeStratergy32(ABC):
+	@abstractmethod
+	def exe_instr(self, instr: uint32, core_state):
+		pass
+			
 
-	def concat_bits(self, instr, *args):
-		return self.bm.concat_bits(instr, args)
-		
-	def get_bits(self, instr, upper, lower=None):
-		return self.bm.get_bits(instr, upper, lower)
-
-	def get_int_bits(self, instr, upper, lower=None):
-		return self.bm.get_int_bits(instr, upper, lower)
-
-	def sign_extend(self, vector, str_len):
-		return self.bm.sign_extend(vector, str_len)
-
-	def pad_0s(self, vector, str_len):
-		return self.bm.pad_0s(vector, str_len)
-
-	def get_2s_comp(self, vector):
-		return self.bm.get_2s_comp(vector)
-
-	def get_sign_extended_int_bits(self, instr, upper, lower, str_len):
-		return self.bm.get_sign_extended_int_bits(instr, upper, lower, str_len)
-
-	def get_0s_padded_int_bits(self, instr, upper, lower, str_len):
-		return self.bm.get_0s_padded_int_bits(instr, upper, lower, str_len)
-
-	def hex2bin(self, hex_str):
-		return self.bm.hex2bin(hex_str)
-
-	def bin2hex(self, vector):
-		pass		
-
-
-class Hex:
-
-	def __init__(self, value : int):
-		self.value = hex(value)
-
-class Memory_32:
+class Memory32:
 		#TODO: finish this write-up...
 	""" This class is used to model 32-bit addressable memory. It has 
 		access to bit manipulation routines via variable "bm". A memory modelA size is specified
@@ -220,7 +26,7 @@ class Memory_32:
 		provide write functionality on a """	
 	def __init__(self, size):
 		self.size = size
-		self.bm = BitManip()
+		self.bm = bm()
 		self.address_space = self.size*[0]
 		self.memory_dump = "mem.dump"
 
@@ -240,4 +46,219 @@ class Memory_32:
 			raise ValueError("data passed is not 32 bits")
 		else:
 			pass
+
+
+class RV32ICORE:
+	""" This class defines the RV32I compatible CORE """ 
+
+	PC: int = 0
+	REG_FILE: List[int] = [0] * 32
+	xlen: int = 32
+
+	def __init__(self, memory: Memory32):
+		self.memory = memory
+
+	def __incr_PC(self, factor: int=0x4):
+		# Store a temporary PC
+		PC = self.PC
+
+		self.PC+=factor
+		if self.PC >= self.mem_size:
+			self.PC = int(0)
+
+		# Return old PC 
+		return PC
+
+	def st_run(self):
+
+		# single thread pipeline:
+
+		# Fetch
+		instr = self.__fetch
+
+		# Decode
+		exe_stratergy = self.__decode(instr)
+
+		# Execute
+		self.__execute(instr, exe_stratergy)
+
+		# Write back ???
+		# No need to implement a seperate stage for write back in model
+		# Execute stratergies can deal with store instructions. 
+			
+	def __fetch(self) -> uint32:
+
+		# Get 32 bit data in memory[PC], then PC++
+		return self.address_space[self.__incr_PC]
+				
+	def __decode(self, instr : uint32) -> InstrExeStratergy32:
+		#TODO: finish this function
+		pass
+
+	def __execute(self, instr: uint32, exe_stratergy: InstrExeStratergy32):
+
+		# Run exe stratergy
+		self = exe_stratergy.exe_instr(instr, self)
+		
+
+class ConditionalBranch_32(InstrExeStratergy32):
+	def exe_instr(self, instr: uint32, core_state: RV32ICORE) -> RV32ICORE:
+
+		opcode, w7 = bm.get_sub_bits_from_instr(instr, 6, 0)
+
+		funct3, w3 = bm.get_sub_bits_from_instr(instr, 14, 12)
+
+		offset, width_offset = bm.concat_bits([
+			bm.get_sub_bits_from_instr(instr, 31, 31),
+			bm.get_sub_bits_from_instr(instr, 8, 8),
+			bm.get_sub_bits_from_instr(instr, 30, 25), 
+			bm.get_sub_bits_from_instr(instr, 11, 8)
+		])
+
+		src1, w5 = bm.get_sub_bits_from_instr(instr, 19, 15)
+		src2, w5 = bm.get_sub_bits_from_instr(instr, 24, 20)
+
+		if core_state.REG_FILE[src1] == core_state.REG_FILE[src2] and funct3 == 0:
+			core_state.__incr_PC(offset)
+
+		elif self.REG_FILE[src1] != core_stateREG_FILE[src2] and funct3 == 1:
+			core_state.__incr_PC(offset)
+
+		elif funct3 == 4 or funct3 == 6:
+			if core_state.REG_FILE[src1] < core_state.REG_FILE[src2]:
+				core_state.incr_PC(offset)
+		
+		elif funct3 == 5 or funct3 == 7:
+			if core_state.REG_FILE[src1] > core_state.REG_FILE[src2]:
+				core_state.incr_PC[offset]
+		
+		return core_state
+
+
+class JumpAndLinkRegsiter_32(InstrExeStratergy32):
+	def exe_instr(self, instr: uint32, core_state: RV32ICORE) -> RV32ICORE:
+		
+		offset, w12 = bm.get_sub_bits_from_instr(instr, 31, 20)
+
+		src, w5 = bm.get_sub_bits_from_instr(instr, 19, 15)
+
+		dst, w5 = bm.get_sub_bits_from_instr(instr, 11, 7)
+
+		core_state.REG_FILE[dst] = core_state.PC + 4
+
+		core_state.PC = offset + src
+	
+		return core_state
+			
+
+class JumpAndLink_32(InstrExeStratergy32):
+	def exe_instr(self, instr: uint32, core_state: RV32ICORE) -> RV32ICORE:
+		# elif bm.opcode == "1101111":
+
+		offset, w20 = bm.get_sub_bits_from_instr([
+			bm.get_sub_bits_from_instr(instr, 31), 
+			bm.get_sub_bits_from_instr(instr, 19, 12), 
+			bm.get_sub_bits_from_instr(instr, 20), 
+			bm.get_sub_bits_from_instr(instr, 30, 21), 
+			bm.get_sub_bits_from_instr(instr, 2)	
+		]) 
+
+		dst, w5 = bm.get_sub_bits_from_instr(instr, 11, 7)
+
+		core_state.REG_FILE[dst] = core_state.PC + 4
+
+		core_state.__incr_PC(offset)
+	
+		return core_state
+
+
+class LoadUpperImm_32(InstrExeStratergy32):
+	def exe_instr(self, instr: uint32, core_state: RV32ICORE) -> RV32ICORE:
+		# elif bm.opcode == "0110111" or bm.opcode == "0010111":
+
+		u_imm = bm.concat_bits([bm.get_sub_bits_from_instr(instr, 31, 12), (0, 12)])[0]
+
+		dst = bm.get_sub_bits_from_instr(instr, 11, 7)[0]
+
+		if bm.opcode == "0110111":
+			core_state.REG_FILE[dst] = u_imm
+		else: 
+			core_state.__incr_PC(u_imm)
+		
+		return core_state
+		
+
+class RegImmInt_32(InstrExeStratergy32):
+	def exe_instr(self, instr: uint32, core_state: RV32ICORE) -> RV32ICORE:
+		# elif bm.opcode == "0010011":
+		
+		funct3, w3 = bm.get_sub_bits_from_instr(instr, 14, 12)
+
+		imm_arith, w12 = bm.get_sub_bits_from_instr(instr, 31, 20)
+
+		imm_shift, w7 = bm.get_sub_bits_from_instr(instr, 31, 25)
+
+		shamt, w5 = bm.get_sub_bits_from_instr(instr, 24, 20)
+
+		src, w5 = bm.get_sub_bits_from_instr(instr, 19, 15)
+
+		dest, w5 = bm.get_sub_bits_from_instr(instr, 11, 7)
+
+		if funct3 == 0:
+			core_state.REG_FILE[dest] = core_state.REG_FILE[src] + core_state.REG_FILE[imm_arith]
+			
+		elif funct3 == 1:	
+			core_state.REG_FILE[dest] = core_state.REG_FILE[src] << shamt
+		
+		elif funct3 == 2:
+			core_state.REG_FILE[dest] = core_state.REG_FILE[src] 
+		#TODO: Finish this routine
+	
+		return core_state
+
+
+class RegRegInt_32(InstrExeStratergy32):
+	def exe_instr(self, instr: uint32, core_state: RV32ICORE) -> RV32ICORE:
+		# elif bm.opcode == "0110011":
+		src2, w5 = bm.get_sub_bits_from_instr(instr, 24, 20)
+		src1, w5 = bm.get_sub_bits_from_instr(instr, 19, 15)
+		dest, w5 = bm.get_sub_bits_from_instr(instr, 11, 7)
+		#TODO: Finish this routine
+	
+		return core_state
+
+
+class Load_32(InstrExeStratergy32):
+	#TODO: finish this routine
+	def exe_instr(self, instr: uint32, core_state: RV32ICORE) -> RV32ICORE:
+		# elif bm.opcode == "0000011":
+		imm = bm.sign_extend_nbit_2_32bit(bm.get_sub_bits_from_instr(instr, 31, 20))
+		src1, w5 = bm.get_sub_bits_from_instr(instr, 19, 15)
+		dest, w5 = bm.get_sub_bits_from_instr(instr, 11, 7)
+		funct3, w3 = bm.get_sub_bits_from_instr(instr, 14, 12)
+		effective_address = core_state.REG_FILE[src1] + imm
+
+		if funct3 == 0:
+			pass
+			# core_state.REG_FILE[dest] = core_state.address_space[effective_address]
+		
+		elif funct3 == 1:
+			#TODO: implement a memory class
+			pass
+	
+		return core_state
+		
+
+class Store_32(InstrExeStratergy32):
+	def exe_instr(self, instr: uint32, core_state: RV32ICORE) -> RV32ICORE:
+		# elif bm.opcode == "0100011":
+	
+		return core_state
+
+
+class Hex:
+
+	def __init__(self, value : int):
+		self.value = hex(value)
+
 
